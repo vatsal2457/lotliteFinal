280ef9c5ec4850a8877b85bb1f33f0ce03bb5708
vatsal-vishal <vatsalvishal003@gmail.com>
2025-03-17 14:36:37 +0530

    remove connect with us

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -72,23 +72,6 @@ const ContactForm = () => {
                 </div>
               </div>
             </div>
-            
-            <div className="mt-12">
-              <p className="font-medium mb-4">Connect With Us</p>
-              <div className="flex space-x-4">
-                {['facebook', 'twitter', 'linkedin', 'instagram'].map((social) => (
-                  <a 
-                    key={social} 
-                    href="#" 
-                    className="bg-white/20 p-2 rounded-full hover:bg-white/30 transition-colors"
-                  >
-                    <svg className="h-5 w-5 text-white" fill="currentColor" viewBox="0 0 24 24">
-                      <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm3 8h-1.35c-.538 0-.65.221-.65.778v1.222h2l-.209 2h-1.791v7h-3v-7h-2v-2h2v-2.308c0-1.769.931-2.692 3.029-2.692h1.971v3z" />
-                    </svg>
-                  </a>
-                ))}
-              </div>
-            </div>
           </div>
           
           <div className="lg:w-2/3 p-8">
